d960ef3f7a6cdb43c23a2e3cbfb9d2033f279586
Julia TkachenkoPyvovarova <yuliia.tkachenko-pyvovarova@privatbank.ua>
2021-12-23 20:42:12 +0200

    commit 23122021

--- src/com/pb/tkachenko/hw11_2/Contact.java
+++ src/com/pb/tkachenko/hw11_2/Contact.java
@@ -1,15 +1,47 @@
 package com.pb.tkachenko.hw11_2;
 
+import java.io.Serializable;
 import java.lang.reflect.Array;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Contact {
-    String FIO;
-    String bDate;
-    String adres;
-    ArrayList<String> tel = new ArrayList<>();
-    LocalDateTime ChangeDate;
+public class Contact implements Serializable {
+    private String FIO;
+    private String bDate;
+    private String adres;
+    private ArrayList<String> tel = new ArrayList<>();
+    private LocalDate ChangeDate;
+
+
+    public Contact(String FIO) {
+        this.FIO = FIO;
+    }
+
+    public Contact(String FIO, String bDate) {
+        this.FIO = FIO;
+        this.bDate = bDate;
+    }
+
+    public Contact(String FIO, String bDate, String adres) {
+        this.FIO = FIO;
+        this.bDate = bDate;
+        this.adres = adres;
+    }
+
+    public Contact(String FIO, String bDate, String adres, ArrayList<String> tel) {
+        this.FIO = FIO;
+        this.bDate = bDate;
+        this.adres = adres;
+        this.tel = tel;
+    }
+
+    public Contact(String FIO, String bDate, String adres, ArrayList<String> tel, LocalDate changeDate) {
+        this.FIO = FIO;
+        this.bDate = bDate;
+        this.adres = adres;
+        this.tel = tel;
+        ChangeDate = changeDate;
+    }
 
     public String getFIO() {
         return FIO;
@@ -17,6 +49,22 @@ public class Contact {
 
     public void setFIO(String FIO) {
         this.FIO = FIO;
+    }
+
+    public String getbDate() {
+        return bDate;
+    }
+
+    public void setbDate(String bDate) {
+        this.bDate = bDate;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
     }
 
     public ArrayList<String> getTel() {
@@ -27,16 +75,13 @@ public class Contact {
         this.tel = tel;
     }
 
-    //Constructor
-    public Contact(String FIO, String BDate, String adres, ArrayList<String> tel, LocalDateTime changeDate) {
-        this.FIO = FIO;
-        this.bDate = bDate;
-        this.adres = adres;
-        this.tel = tel;
-        ChangeDate = changeDate;
+    public LocalDate getChangeDate() {
+        return ChangeDate;
+    }
 
-        System.out.println(FIO + ", " + bDate + ", " + adres + ", " + tel + ", " + ChangeDate);
-        }
+    public static void setChangeDate(LocalDate changeDate) {
+        ChangeDate = changeDate;
+    }
 
     @Override
     public String toString() {
--- src/com/pb/tkachenko/hw11_2/TelefonMain.java
+++ src/com/pb/tkachenko/hw11_2/TelefonMain.java
@@ -3,6 +3,7 @@ package com.pb.tkachenko.hw11_2;
 import javafx.beans.binding.MapExpression;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -12,15 +13,17 @@ import static sun.nio.ch.IOUtil.load;
 public class TelefonMain {
 
     private static Object String;
-    
+
+    public TelefonMain() throws IOException {
+    }
+
     public static void main(String[] args) throws Exception {
 
-        //
-        List<String> tel = Arrays.asList("0677029726", "0677654321", "09956423", "098765431");
+        List<String> tel = Arrays.asList();
         ArrayList<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact("Ткаченко Юлия Васильевна", "11.03.1981", "Днепр", "0677029726", LocalDateTime.of(1998, 03, 11)));
-        contacts.add(new Contact("Иванов Иван Иванович", "14.03.2003" , "Киев", "0677654321", LocalDateTime.of(1997, 03, 14)));
-        contacts.add(new Contact("Петров Петр Петрович", "11.12.2017", "Запорожье", "09956423, 098765431", LocalDateTime.of(1995, 12,11)));
+        contacts.add(new Contact("Ткаченко Юлия Васильевна", "11.03.1981", "Днепр", "0677029726", LocalDate.of(1998, 03, 11)));
+        contacts.add(new Contact("Иванов Иван Иванович", "14.03.2003", "Киев", "0677654321", LocalDate.of(1997, 03, 14)));
+        contacts.add(new Contact("Петров Петр Петрович", "11.12.2017", "Запорожье", "09956423, 098765431", LocalDate.of(1995, 12, 11)));
 
         //добавление элемента
         Scanner scan = new Scanner(System.in);
@@ -31,9 +34,9 @@ public class TelefonMain {
         System.out.print("Введите дату рождения: ");
         String newBDate = scan.nextLine();
         System.out.print("Введите телефон: ");
-        String newTel = scan.nextLine();
+        List<String> newTel = Collections.singletonList(scan.nextLine());
 
-        Contact newContact = new Contact(newFIO, newBDate, newAdres, newTel);
+        Contact newContact = new Contact(newFIO, newBDate, newAdres, (ArrayList<java.lang.String>) newTel);
         contacts.add(newContact);
 
         //удаление элемента: принимает телефон
@@ -44,8 +47,8 @@ public class TelefonMain {
 
         //поиск элементов
         System.out.println("Введите телефон для поиска контактов: ");
-        String findTel = scan.nextLine();
-        Contact result = contacts.get(tel);
+        Integer findTel = Integer.valueOf(scan.nextLine());
+        Contact result = contacts.get(findTel);
         if (result != null) {
             System.out.println("Поиск по номеру телефона: телефон " + findTel + " принадлежит " + result.FIO);
         } else
@@ -56,54 +59,115 @@ public class TelefonMain {
         // Сортировка по FIO
         System.out.println("Вывод всех контактов с сортировкой по ФИО. ");
         //Comparator<Message> comparator = Comparator.comparing(obj -> obj.getId());
-        contacts.sort(Comparator.comparing(obj -> obj.FIO));
+        contacts.sort(Comparator.comparing(obj -> obj.getFIO()));
         System.out.println("Сортировка по ФИО:" + contacts);
 
         // Сортировка по adres
         System.out.println("Вывод всех контактов с сортировкой по адресу. ");
-        contacts.sort(Comparator.comparing(obj -> obj.adres));
+        contacts.sort(Comparator.comparing(obj -> obj.getAdres()));
         System.out.println("Сортировка по адресу: " + contacts);
 
         //редактирование элемента
+        Scanner Input = new Scanner(System.in);
+        int Punkt = 0;
+        while (true) {
+            System.out.print("Что нужно изменить?");
+
+            System.out.print("ФИО? - 1");
+            System.out.print("Дата рождения? - 2");
+            System.out.print("Адрес? - 3");
+            System.out.print("Телефон? - 4");
+            System.out.print("Выход - 0");
+            Punkt = Input.nextInt();
+
+            if (Punkt == 0) {
+                System.out.println("Выход");
+                break;
+            } else
+            {switch (Punkt){
+                case 1 -> {
+                    System.out.println("Введите ФИО: ");
+                    String FIOnew = Input.nextLine();
+                    Contact.setFIO(FIOnew);
+                    break;
+                }
+                case 2 -> {
+                    System.out.print("Введите дату рождения: ");
+                    String BDateNew = Input.nextLine();
+                    Contact.setbDate(BDateNew);
+                    break;
+                }
+                case 3 -> {
+                    System.out.print("Введите адрес: ");
+                    String AdresNew = Input.nextLine();
+                    Contact.setAdres(AdresNew);
+                    break;
+                }
+                case 4 -> {
+                    System.out.print("Введите телефон: ");
+                    String TelNew = Input.nextLine();
+                    Contact.setTel(TelNew);
+                    break;
+                }
+            }
+            }
+        }
+
+
+
 
 
 
 
         //запись в файл всех данных
-        System.out.println("");
-        saveTel();
+        //System.out.println("");
+        //saveTel();
 
         //загрузка из файла всех данных
-        System.out.println("");
-        loadTel();
+        //System.out.println("");
+        //loadTel();
+
+
+        //запись в файл всех данных 2
+        File file = Paths.get("TelBook.txt").toFile();
+        FileOutputStream outputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        // сохраняем в файл
+        objectOutputStream.writeObject(contacts);
+        //закрываем поток и освобождаем ресурсы
+        objectOutputStream.close();
+        System.out.println("Файл записан");
+
+
+        File file2 = Paths.get("files/person.data").toFile();
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        List<Contact> contacts1 = (List<Contact>) objectInputStream.readObject();
+        System.out.println(contacts1);
+
+        }
+
 
     }
-
-        //запись в файл всех данных: saveTel2
-
-
-
-
         //запись в файл всех данных: saveTel в текстовом файле phone.txt --метод
-        private static void saveTel() throws Exception{
-        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("phone.txt")));
-        for(Map.Entry<String,String> k: tel.entrySet()){
-        writer.write(k.getKey() + " " + k.getValue()+System.lineSeparator());
-        }
-        writer.close();
-        }
+        //private static void saveTel() throws Exception{
+        //BufferedWriter writer = new BufferedWriter(new FileWriter(new File("phone.txt")));
+        //for(Map.Entry<String,String> k: tel.entrySet()){
+        //writer.write(k.getKey() + " " + k.getValue()+System.lineSeparator());
+        //}
+        //writer.close();
+        //}
 
         //загрузка из файла всех данных: из текстового файла phone.txt -- метод
-        public static void loadTel() throws Exception {
-            File file = new File("phone.txt");
-            if (file.exists()){
-                BufferedReader reader = new BufferedReader(new FileReader(new File("phone.txt")));
-                String ttt;
-                while ((ttt=reader.readLine())!=null) {
-                    String[] dat = ttt.split(" ");
-                    tel.add(Integer.parseInt(dat[0]), dat[1]);
-                }
-                reader.close();
-            }
-        }
-    }
+        //public static void loadTel() throws Exception {
+        //    File file = new File("phone.txt");
+        //    if (file.exists()){
+        //        BufferedReader reader = new BufferedReader(new FileReader(new File("phone.txt")));
+        //        String ttt;
+        //        while ((ttt=reader.readLine())!=null) {
+        //            String[] dat = ttt.split(" ");
+        //            tel.add(Integer.parseInt(dat[0]), dat[1]);
+        //        }
+        //        reader.close();
+        //    }
+        //}
