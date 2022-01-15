00b579148946f850708c565aece640e88757a232
Julia TkachenkoPyvovarova <yuliia.tkachenko-pyvovarova@privatbank.ua>
2022-01-15 14:04:27 +0200

    commit 15012022++

--- src/com/pb/tkachenko/hw9/FileNumbers.java
+++ src/com/pb/tkachenko/hw9/FileNumbers.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Random;
 import java.util.Scanner;
 
 public class FileNumbers {
@@ -11,22 +12,33 @@ public class FileNumbers {
     // 10 строк и по 10 чисел в каждой строке. Числа разделять пробелами.
 
     public static void createNumbersFile() throws IOException {
-        try {
-            Writer writer = new FileWriter("D:\\Users\\dn110381tjv\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\tkachenko\\hw9\\numbers.txt");
-            int[] array = new int[100];
-            for (int i = 0; array.length > i; i++) {
-                array[i] = (int) (Math.random() * (100 - 1) + 1);
-                System.out.println();
-                writer.write(array[i] + " ");
-                System.out.print(array[i] + " ");
-                if ((i + 1) % 10 == 0) {
-                    writer.write("\n");
-                }
-            }
-            writer.close();
+        Path path1 = Paths.get("numbers.txt");
 
+        try (BufferedWriter writer = Files.newBufferedWriter(path1)) {
+            for (int i = 0; i < 10; i++) {
+                    int[] arrInt = new int[10];
+                    Random random = new Random();
+                    for (int el = 0; el < 10; el++) {
+                        arrInt[el] = random.nextInt(100);
+                        if(arrInt[el] >= 10) writer.write(arrInt[el] + " ");
+                        else writer.write("0"+arrInt[el] + " ");
+                    }
+                    writer.newLine();
+                }
+
+            //int[] array = new int[100];
+            //for (int i = 0; array.length > i; i++) {
+            //    array[i] = (int) (Math.random() * (100 - 1) + 1);
+            //    writer.write(array[i] + " ");
+            //    System.out.print(array[i] + " ");
+            //    if ((i + 1) % 10 == 0) {
+            //        writer.write("\n");
+            //   }
+            //    System.out.println();
+            //}
+            //writer.close();
         } catch (IOException io) {
-            System.out.println(io);
+            System.out.println("Ошибка записи: " + io);
         }
     }
 
@@ -34,40 +46,48 @@ public class FileNumbers {
 // в который входят все числа из "numbers.txt" только все четные заменены на 0.
 
     public static void createOddNumbersFile() throws IOException {
-        Path path1 = Paths.get("D:\\Users\\dn110381tjv\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\tkachenko\\hw9\\numbers.txt");
+        Path path21 = Paths.get("numbers.txt");
+        Path path22 = Paths.get("odd-numbers.txt");
         // read from file
-        try (BufferedReader reader = Files.newBufferedReader(path1)) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+        try (BufferedReader reader = Files.newBufferedReader(path21);
+             BufferedWriter writer = Files.newBufferedWriter(path22)) {
+            String a;
+            int q = 0;
+            while ((a = reader.readLine()) != null) {
+                System.out.println(a);
+                String[] arr = a.split(" ");
+                int[] intArr = new int[arr.length];
+                for (int i = 0; i < arr.length; i++) {
+                    int intVal = Integer.parseInt(arr[i]);
+                    if (intVal % 2 == 0) {
+                        intArr[i] = 0;
+                        q++;
+                    } else intArr[i] = intVal;
+                    if (intArr[i] >= 10) writer.write(intArr[i] + " ");
+                    else writer.write("0" + intArr[i] + " ");
+                }
+                writer.newLine();
             }
-        } catch (IOException io) {
-            System.out.println("Ошибка чтения файла: " + io);
+        } catch (Exception io) {
+            System.out.println("Ошибка: " + io);
         }
-        System.out.println("Файл прочитан!");
+        System.out.println("Файл записан!");
+    };
 
-        // write to file
-        Path path2 = Paths.get("D:\\Users\\dn110381tjv\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\tkachenko\\hw9\\numbers.txt");
-        try (BufferedWriter writer = Files.newBufferedWriter(path2)) {
-            //int c = str.nextInt();
-           // if (c % 2 != 0) {
-                //System.out.println("0");
-               // writter2.write(c + " ");
-           // } else {
-                //System.out.println(c + " ");
-              //  c = 0;
-             //   writter2.write(c + " ");
+    static void readPath(Path pathPath) {
+        Path path = pathPath;
+        System.out.println("Читаем файл \"" + path.toAbsolutePath() + "\":");
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String d;
+            while ((d = reader.readLine()) != null) {
+                System.out.println(d);
             }
-         //   writer.write("Some test 123");
-       //     writer.newLine();
-     //       writer.write("Other string 456");
-      //  } catch (Exception ex) {
-        //    System.out.println("Error with file write: " + ex);
+        } catch (Exception ex) {
+            System.out.println("Ошибка: " + ex);
         }
-        //System.out.println("Write to file \"" + path.toAbsolutePath() + "\" done!");
-        }
-
-
+        System.out.println("Файл прочитан: " + pathPath.toString());
+    };
+}
 
 
 
--- src/com/pb/tkachenko/hw9/main.java
+++ src/com/pb/tkachenko/hw9/main.java
@@ -1,6 +1,10 @@
 package com.pb.tkachenko.hw9;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.pb.tkachenko.hw9.FileNumbers.readPath;
 
 public class main {
     public main() throws IOException {
@@ -8,6 +12,9 @@ public class main {
 
     public static void main(String[] args) throws Exception {
         FileNumbers.createNumbersFile();
+        Path path = Paths.get("files/numbers.txt");
+        readPath(path);
+
         FileNumbers.createOddNumbersFile();
     }
 }
